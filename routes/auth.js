3290d8a165549e72a4c3585ce7a0113af5f041c2
kurooo23 <bagas.yoga500@gmail.com>
2026-04-07 22:30:01 +0800

    test

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,7 @@ router.post("/register", async (req, res) => {
     const statusCode = result.status === "error" ? 500 : 201;
     return res.status(statusCode).json(result);
 });
+// router.
 
 
 export default router;
